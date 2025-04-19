8920150ea748b6774599e3c7332aaab3ae1a512b
rakeshcooper <rakeshmurali097@gmail.com>
2025-04-20 01:39:51 +0530

    Implementing vueTodo

--- src/components/VueTodo.vue
+++ src/components/VueTodo.vue
@@ -1,27 +1,47 @@
 <template>
     <div>
         <h1>Todo</h1>
-        <input type="text" name="inputTodo" id="inputTodo">
-        <button>Add</button>
-        <ul></ul>
-        <button class="btn"  @click="countIncrement">count :{{ count }}</button>
+        <input @input="getInputvalue" type="text" name="inputTodo" id="inputTodo">
+        <button @click="addHandler">Add</button>
+        <ul>
+            <li :key="dat" v-for="dat in data">
+                <span>{{ dat.todo }}</span>
+                <span><input type="text"><button>update</button></span>
+                <span><button>edit</button></span>
+                <span><button>delete</button></span>
+                <span><button>done</button></span>
+            </li>
+        </ul>
 
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
+let cryptoId 
+let newData 
+let data = reactive([])
+let setData
 
-let count = ref(0)
 
-function countIncrement(){
-    count.value +=1
-
+function addHandler(){
+    cryptoId = crypto.randomUUID()
+    setData = { todo: newData ,ID: cryptoId, isChecked: false, isDeleted: false}
+    console.log(setData);
+    data.push(setData)
 }
+
+
+function getInputvalue(e){
+    console.log(e.target.value);
+    newData = e.target.value
+}
+
+
 </script>
 
 <style lang="scss" scoped>
-    .btn{
-        
+    button{
+        cursor: pointer;
     }
 </style>
