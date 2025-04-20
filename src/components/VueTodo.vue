fe3d385d7055dfaac8ca8c7ba6eba604f52bbf64
rakeshcooper <rakeshmurali097@gmail.com>
2025-04-21 01:12:57 +0530

    New update for Crud

--- src/components/VueTodo.vue
+++ src/components/VueTodo.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
         <h1>Todo</h1>
-        <input @input="getInputvalue" type="text" name="inputTodo" id="inputTodo">
+        <input ref="mainInput" @input="getInputvalue" type="text" name="inputTodo" id="inputTodo">
         <button @click="addHandler">Add</button>
         <ul>
             <li :key="dat" v-for="dat in data">
-                <span>{{ dat.todo }}</span>
-                <span v-if="dat.isEdited"><input @input="updateInputvalue" type="text"><button @click="updateHandler(dat.ID)">update</button></span>
+                <span :class="{checked: dat.isChecked}">{{ dat.todo }}</span>
+                <span v-if="dat.isEdited"><input v-model="dat.todo" @input="updateInputvalue" type="text"><button @click="updateHandler(dat.ID)">update</button></span>
                 <span><button @click="editHandler(dat.ID)">{{dat.isEdited ? "cancel" : "edit"}}</button></span>
                 <span><button @click="deleteHandler(dat.ID)">delete</button></span>
-                <span><button>{{dat.isChecked ? "undone" : "done"}}</button></span>
+                <span><button @click="checkHandler(dat.ID)">{{dat.isChecked ? "undone" : "done"}}</button></span>
             </li>
         </ul>
 
@@ -17,18 +17,20 @@
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive, watch } from 'vue';
 let cryptoId 
 let newData 
-let data = reactive([])
+let data = reactive(JSON.parse(localStorage.getItem("VuetestItems") ?? "[]"))
 let setData
+let mainInput = ref(null)
 
 
 function addHandler(){
     cryptoId = crypto.randomUUID()
-    setData = { todo: newData ,ID: cryptoId, isChecked: false, isEdited: false}
+    setData = { todo: newData ,ID: cryptoId, isChecked: false, isEdited: false }
     console.log(setData);
     data.push(setData)
+    mainInput.value.value = ""
 }
 
 
@@ -61,9 +63,25 @@ function updateHandler(ID){
 
 function deleteHandler(ID){
     data.forEach((element,index) => {
-        data.splice(index, 1)
+        if(element.ID == ID){
+            data.splice(index, 1)
+        }
     })
 }
+
+function checkHandler(ID){
+    data.forEach((element,index) => {
+        if(element.ID == ID){
+            data[index] = { ...element, isChecked: !element.isChecked }
+            console.log( data[index]);
+            
+        }
+    })
+}
+
+watch(() => data, (newVal) => {
+    localStorage.setItem("VuetestItems", JSON.stringify(newVal))
+}, { deep: true })
 
 
 </script>
@@ -71,5 +89,13 @@ function deleteHandler(ID){
 <style lang="scss" scoped>
     button{
         cursor: pointer;
+    }
+
+    .checked{
+        text-decoration: line-through;
+    }
+
+    .unchecked{
+        text-decoration: none;
     }
 </style>
